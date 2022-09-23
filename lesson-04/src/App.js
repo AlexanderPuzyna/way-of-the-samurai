4fbb4857786ded41cf2a0c9a6bdf81f70b1f2f67
Alexander Puzyna <alexanderpuzyna@MacBook-Pro-Aleksandr.local>
2022-09-23 18:10:31 +0300

    lesson-11

--- lesson-04/src/App.js
+++ lesson-04/src/App.js
@@ -3,33 +3,50 @@ import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-      <Technologies/>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://www.websitegrowth.com/wp-content/uploads/2016/08/apple-logo-300x300.png'/>
+      </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+        
+      </nav>
+      <div className='content'>
+        <div>
+        <img src='https://www.ukinbound.org/wp-content/uploads/2020/07/We-Are-Tourism-BCP-875x350.jpg'/>
+        </div>
+        <div>
+          ava+description
+        </div>
+        <div>
+          My posts
+          <div>
+          New Post
+        </div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-const Technologies = () => {
-  return (
-    <div>
-      <ul>
-        <li>html</li>
-        <li>css</li>
-        <li>js</li>
-      </ul>
-    </div>
-  )
-}
 
-const Header = () => {
-  return (
-    <div>
-      <a href='#'>Home</a>
-      <a href='#'>News Feed</a>
-      <a href='#'>Messages</a>
-    </div>
-  );
-}
 
 export default App;
