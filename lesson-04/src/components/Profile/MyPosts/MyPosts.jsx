42636d45f64f1ad8c832a968528010090813bb5e
Alexander Puzyna <alexanderpuzyna@MacBook-Pro-Aleksandr.local>
2022-10-08 12:23:48 +0300

    “lesson17”

--- lesson-04/src/components/Profile/MyPosts/MyPosts.jsx
+++ lesson-04/src/components/Profile/MyPosts/MyPosts.jsx
@@ -12,7 +12,7 @@ const MyPosts = () => {
 
       </div>
       <div className={s.posts}>
-        <Post message ="Hi, how are you?" likesCount ="15"/>
+        <Post message ="Hi, how are you?" likesCount ="25"/>
         <Post message ="It's my first post"likesCount ="20"/>
       </div>
     </div>);
