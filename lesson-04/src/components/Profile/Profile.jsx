18939f74dc632ce4b0d3967d35c973d06ff88854
Alexander Puzyna <alexanderpuzyna@MacBook-Pro-Aleksandr.local>
2022-10-08 13:33:22 +0300

    lesson-18

--- lesson-04/src/components/Profile/Profile.jsx
+++ lesson-04/src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from './MyPosts/MyPosts';
 import s from'./Profile.module.css';
 
 const Profile = () => {
-    return  <div className={s.content}>
+    return  <div>
     <div>
       <img src='https://www.ukinbound.org/wp-content/uploads/2020/07/We-Are-Tourism-BCP-875x350.jpg' />
     </div>
