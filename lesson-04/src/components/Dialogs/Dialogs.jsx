4f60b5dd7a36fcccd83581cbad6404290fc32c1c
Alexander Puzyna <alexanderpuzyna@MacBook-Pro-Aleksandr.local>
2022-10-17 12:24:55 +0300

    lesson-26

--- lesson-04/src/components/Dialogs/Dialogs.jsx
+++ lesson-04/src/components/Dialogs/Dialogs.jsx
@@ -1,51 +1,37 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import DialogItem from "./DialogItem/DialogItem";
 import s from './Dialogs.module.css';
-
-const DialogItem = (props) => {
-    let path = '/dialogs/'+ props.id;
-    return <div className={s.dialog + " " +s.active}>
-    <NavLink to={path}>{props.name}</NavLink>
-</div>
-}
-
-const Message = (props) => {
-    return <div className={s.message}>{props.message}</div>
-}
+import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 
     let dialogs = [
-        {id: 1, name: 'Алекс Портареску'}, 
-        {id: 2, name: 'Максим Озерский'},
-        {id: 3, name: 'Яна Пузына'},
-        {id: 4, name: 'Армин Ван Бюрен'},
-        {id: 5, name: 'Кто-то'}
+        { id: 1, name: 'Алекс Портареску' },
+        { id: 2, name: 'Максим Озерский' },
+        { id: 3, name: 'Яна Пузына' },
+        { id: 4, name: 'Армин Ван Бюрен' },
+        { id: 5, name: 'Кто-то' }
     ];
 
     let messages = [
-        {id: 1, message: 'Hi'}, 
-        {id: 2, message: 'How are you?'},
-        {id: 3, message: 'Its OK?'},
-        {id: 4, message: 'Хэй бро, когда мой гост допишешь?'},
-        {id: 5, message: 'Ок'}
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'How are you?' },
+        { id: 3, message: 'Its OK?' },
+        { id: 4, message: 'Хэй бро, когда мой гост допишешь?' },
+        { id: 5, message: 'Ок' }
     ];
 
-    let dialogsElements = dialogs.map( d => <DialogItem name={d.name} id={d.id}/> )
-
-    
-
-    let messagesElements = messages.map( m => <Message message={m.message}/> )
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id} />)
+    let messagesElements = messages.map(m => <Message message={m.message} />)
 
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
                 {dialogsElements}
-                 
+
             </div>
             <div className={s.messages}>
                 {messagesElements}
-                
             </div>
         </div>
     )
