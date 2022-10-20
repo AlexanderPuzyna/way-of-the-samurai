b8056ed8510b43c4370eead7f3d507a4b0823950
Alexander Puzyna <alexanderpuzyna@MacBook-Pro-Aleksandr.local>
2022-10-20 20:22:46 +0300

    lesson-32

--- lesson-04/src/components/Profile/MyPosts/Post/Post.jsx
+++ lesson-04/src/components/Profile/MyPosts/Post/Post.jsx
@@ -4,7 +4,7 @@ import s from'./Post.module.css';
 const Post = (props) => {
     return(  
         <div className={s.item}>
-        <img src='https://cf.shopee.co.id/file/3d5fdb772608ecc4032245ec444da42a'/>
+        <img src='https://sun9-east.userapi.com/sun9-18/s/v1/if1/1qzBrqLOJRpROOSGzSQYx9-8gmZxBbdf1QJz4EoCh691sC1Ee-VDQi-7W4GtjSfhThwCuL6B.jpg?size=1280x1280&quality=96&type=album'/>
           {props.message}
           <div>
           <span>like</span>{props.likesCount}
